53a1c94882bebda1cc7ecd00ccc020c41f8a297b
Chenwei Zhu <cz1895@nyu.edu>
2023-11-05 03:29:16 -0500

    HW9

--- README.md
+++ README.md
@@ -1,60 +1,14 @@
-# p5.js Template
+# HW09
+I choose the hand pose form ml5
+https://learn.ml5js.org/#/reference/handpose
 
-This is a README file that can be used to describe and document your assignment.
+I am currently taking an Augmented Reality course, so I'm very interested in creating a gesture-controlled game. After some research, I learned that a viable method is to record and compare the positions of various points on the fingers to determine gestures. Initially, I wanted to create a game where making a gun gesture would shoot little balls, but I found that since the thumb has a wide range of motion and it's short, it's difficult to judge distances and often fails to receive commands correctly. So, I ended up using the ring finger to control gesture changes (as the last three fingers almost move together). A ball is shot when the tip of the ring finger is lower than the knuckle. Players can hit targets with the ball to score points.
 
-Markdown Cheatsheet (from [https://www.markdownguide.org/cheat-sheet/](https://www.markdownguide.org/cheat-sheet/)):
+I think using someone else's code is an exciting process, but it also brings challenges and difficulties. For instance, I spent quite some time understanding how exactly to listen for position information of specific points, and so on...
 
----
----
-
-# Heading1
-## Heading2
-### Heading3
-#### Heading4
-##### Heading5
-###### Heading6
-
-**bold text**
-
-*italicized text*
-
-~~strikethrough text~~
-
-Ordered List:
-1. First item
-2. Second item
-3. Third item
-
-Unordered List:
-- First item
-- Second item
-- Third item
-
-`short code block`
-
-```
-extended code block
-fun() {
-  return 0
-}
-```
-
-Link:  
-[linked text](https://www.example.com)
-
-
-Image with url:  
-![image description](https://dm-gy-6063-2023f-d.github.io/assets/homework/02/clark-espaco-modulado-00.jpg)
 
 
 Image on repo:  
-![image description](./file-name.jpg)
+![image description](./instruction.jpg)
 
 
-To start a new line, add two spaces at the end of a line, like this:  
-this is a new line.
-
-
-To start a new paragraph, leave an empty line between two lines of text.
-
-This is a new paragraph.
--- sketch.js
+++ sketch.js
@@ -1,6 +1,155 @@
+// Define global variables
+let handpose;
+let video;
+let predictions = [];
+let canvas2;
+let pointerX, pointerY; // Position of the index fingertip (index 8)
+let knuckle, ring; // Positions of ring finger knuckle and the its tip
+let blueBalls = []; // Array to hold blue balls (bullets)
+let redRect; // The red rectangle (enemy)
+let hitCount = 0; // Number counting for the number of hittings
+let shootCooldown = 0; // "Cooldown" for shooting blue balls
+
+
+// A function to draw keypoints and UFO (on index finger tip)
+function drawKeypoints() {
+  for (let i = 0; i < predictions.length; i += 1) {
+    const prediction = predictions[i];
+
+    // Loop through all the keypoints on hand
+    for (let j = 0; j < prediction.landmarks.length; j += 1) {
+      const keypoint = prediction.landmarks[j];
+      // Draw an ellipse and number of index for each keypoint
+      fill(173, 216, 230);
+      noStroke();
+      const x = keypoint[0];
+      const y = keypoint[1];
+      ellipse(x, y, 10);
+      fill(255);
+      textSize(10);
+      text(j, x + 4, y);
+      
+      // Draw the UFO at the position of index 8
+      if (j === 8) {
+        pointerX = keypoint[0];
+        pointerY = keypoint[1];
+        drawUFO(pointerX, pointerY); // Draw UFO using the custom function
+      }
+      //key function: detecting bending finger:
+      //compare knuckle and ring finger tip positions
+      if (j === 14) {//check and record y position of the index if it is index 14 (knuckle of ring finger)
+        knuckle = keypoint[1];
+      } else if (j === 16) {//check and record y position of the index if it is index 16 (tip of ring finger)
+        ring = keypoint[1];
+      }
+    }
+
+    // If the ring finger tip is lower than the knuckle, meaning that the user is holding a fist (somehow a fist?) and when cool down is ready, then shoot!
+    if (knuckle < ring && shootCooldown === 0) {
+      blueBalls.push({ x: pointerX, y: pointerY, vx: 10 }); // Add a new blue ball shoot out from index finger tip
+      shootCooldown = 30; // Reset cooldown to 30 frames after shooting
+    }
+  }
+}
+
+// Function to draw the blue balls
+function drawBlueBalls() {
+  for (let i = blueBalls.length - 1; i >= 0; i--) {
+    const ball = blueBalls[i];
+    // Move the ball based on the velocity
+    ball.x += ball.vx; 
+    // Draw the ball
+    fill(0, 0, 255);
+    ellipse(ball.x, ball.y, 10);
+    
+    // Collision detection of bullet and red rectangle
+    if (ball.x > redRect.x && ball.x < redRect.x + redRect.w &&
+        ball.y > redRect.y && ball.y < redRect.y + redRect.h) {
+      blueBalls.splice(i, 1); // Remove the blue ball if collision
+      redRect = createRedRect(); // Create a new red rectangle
+      hitCount++; // Increment the hit count
+      continue; //I did some research to learn this, this means that if this happen the following code would be skip and we are return to next iteration
+    }
+    
+    // Remove the ball if it goes off-screen
+    if (ball.x > width || ball.y > height || ball.y < 0) {
+      blueBalls.splice(i, 1);
+    }
+  }
+}
+
+// Function to draw the red rectangle
+function drawRedRect() {
+  fill(255, 0, 0);
+  rect(redRect.x, redRect.y, redRect.w, redRect.h);
+}
+
+// Function to display the hit count number
+function displayHitCount() {
+  push(); // Save the current drawing state
+  fill(255);
+  textSize(32);
+  // Flip the text horizontally
+  scale(-1.0, 1.0);
+  text(`Hits: ${hitCount}`, -width + 150, 50);
+  pop();
+}
+
+// Function to create new red rectangle object
+function createRedRect() {
+  // random but with restristions
+  let rectX = random(width / 2 + 40, width - 40);
+  let rectY = random(40, height - 80);
+  let rectW = random(20, 60);
+  let rectH = random(20, 60);
+
+  return { x: rectX, y: rectY, w: rectW, h: rectH };
+}
+
+// Custom function to draw the UFO at the given position (x, y)
+function drawUFO(x, y) {
+  fill(173, 216, 230);
+  ellipse(x, y, 30);
+  fill(128, 128, 128);
+  ellipse(x, y, 40, 10);
+  fill(173, 216, 230);
+  for (let offset of [-10, 0, 10]) {
+    ellipse(x + offset, y, 7);
+  }
+}
+
+
+
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  createCanvas(640, 480);
+  video = createCapture(VIDEO);
+  video.size(width, height);
+
+  // Initializing the handpose model here
+  handpose = ml5.handpose(video, modelReady);
+  // This will track the predictions from the handpose model, this system is borrowed from the ml5 website. When the "predict" event occurs, the provided callback function is called.
+  handpose.on('predict', function (results) {
+    predictions = results;//assigns the tracking results to the global predictions
+  });
+  redRect = createRedRect(); // Initialize the red rectangle object
+}
+
+// Function to be called when the handpose model is loaded
+function modelReady() {
+  console.log('Model ready!');
 }
 
 function draw() {
+  translate(width, 0); // Flip x-axis to act as a mirror
+  scale(-1.0, 1.0);
+  image(video, 0, 0, width, height); // Draw the video
+  drawKeypoints(); // Function to draw keypoints of hand
+  drawBlueBalls(); // Function to draw blue balls
+  drawRedRect(); // Function to draw the red rectangle
+  displayHitCount(); // Function to display the hit count number
+
+  // Decrease the bullet cooldown if it's greater than 0
+  if (shootCooldown > 0) {
+    shootCooldown--;
+  }
 }
